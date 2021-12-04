d23247c1df409cea497eaa81cfada048941abc28
George Spearing <spearingg@gmail.com>
2021-12-04 16:30:23 -0500

    adding comments and return data

--- src/loadEEGData.py
+++ src/loadEEGData.py
@@ -9,11 +9,30 @@ Created on Wed Dec  1 14:43:03 2021
 import numpy as np
 
 def loadData(subject=1, series=1, data_directory='train'):
+    '''
+    load in one series from one subject. 
+
+    Parameters
+    ----------
+
+    subject : int, optional
+        The subject to analyze, value of 1:12 accepted, default is 1.
+    series : int, optional
+        The series to analyze, value of 1:8 accepted, default is 1.
+    data_directory : string, optional
+        The directory location for data, default is '../train'
+
+    Returns
+    -------
+    data : Dictionary
+        Dict of raw eeg values and channel names
+
+    '''
     
     file_path = f'{data_directory}/subj{subject}_series{series}_data.csv'
     
     # load just the data
-    eegData = np.loadtxt(file_path, delimiter=',', usecols=(np.arange(1,33)), max_rows=1)
+    eegData = np.loadtxt(file_path, delimiter=',', usecols=(np.arange(1,33)), skiprows=1)
     
     # just the channel names
     channels = np.loadtxt(file_path, delimiter=',', usecols=(np.arange(1,33)), max_rows=1, dtype=str, unpack=True)
@@ -22,3 +41,6 @@ def loadData(subject=1, series=1, data_directory='train'):
     data = {}
     data['eeg'] = eegData
     data['channels'] = channels
+
+    # return the data
+    return data
