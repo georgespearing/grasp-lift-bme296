0e8c9bfaa734c4ab0430673296f140e6aaa09363
George Spearing <spearingg@gmail.com>
2021-12-04 16:30:38 -0500

    initializing files

---
+++ src/helper_grasp_lift.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+helper_grasp_lift.py
+
+functions to help with data analysis. this includes epoching
+data, making classifications, predictors and plotting results
+
+@author: george
+"""
+
+
+# %% EPOCH THE DATA
+
+
+#%% CLASSIFICATIONS
+
+#%% PREDICTIONS
+
+#%% RESULTS
---
+++ src/main_grasp_lift.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+main_grasp_lift.py
+
+Main driver file for the grasp lift function library. 
+Load in the data, epoch all the data, filter results, run ML
+classification and make predictions of actions
+
+
+@author: george
+"""
+
+# %% IMPORTS
+import loadEEGData as eegdata
+
+# %% CONSTANTS
+
+DEFAULT_DIRECTORY = '../train' # default relative location to the training data
+DEFAULT_SUBJECT = 1 # which subject to use, 1:12
+DEFAULT_SERIES = 1 # which series to use, 1:8
+
+# %% LOAD IN DATA
+
+data = eegdata.loadData(subject=DEFAULT_SUBJECT, series=DEFAULT_SERIES, data_directory=DEFAULT_DIRECTORY)
+
+# %% EPOCH THE DATA
+
+#%% CLASSIFICATIONS
+
+#%% PREDICTIONS
+
+#%% RESULTS
